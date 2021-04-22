22b80aefceee70c396725ee6d3dc6e1ad5761c26
Vladimir <freshcode.yt@gmail.com>
2021-04-22 16:34:07 +0300

    Fix start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Билдим образ на основе докер-файла
 sudo docker build -t app:latest -f ./Dockerfile .
-sudo docker run -dit -p 8080:3000 --name my_server app
+# Запускаем контейнер на основе созданного образа
+sudo docker run -dit -p 8080:3000 -v $(pwd):/server --name my_server app
